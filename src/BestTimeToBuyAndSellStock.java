25342f7bdbad846d9c9790084c029da95c0d1e2e
ZacharyBao <tungpao0707@gmail.com>
2020-10-31 23:08:31 +1000

    add BestTimeToBuyAndSellStock

---
+++ src/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,17 @@
+/**
+ * Created by dbaofd on 2020/10/31.
+ */
+public class BestTimeToBuyAndSellStock {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                int profit = prices[j] - prices[i];
+                if (profit > maxProfit) {
+                    maxProfit = profit;
+                }
+            }
+        }
+        return maxProfit;
+    }
+}
