10e8db8c377ba2ebeeeee4c6b416f5396ec1c8ea
ZacharyBao <tungpao0707@gmail.com>
2020-12-26 09:50:52 +1000

    add DeleteNodeInALinkedList

---
+++ src/DeleteNodeInALinkedList.java
@@ -0,0 +1,9 @@
+/**
+ * Created by dbaofd on 2020/12/26.
+ */
+public class DeleteNodeInALinkedList {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
